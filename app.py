ce84bb722e87ccc6a0d58a26b081c58056696584
Jayadhiman31 <101348878+Jayadhiman31@users.noreply.github.com>
2025-01-29 21:48:26 +0530

    app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
+import os
+
+
+secret_key = os.getenv("SECRET_KEY")
 
 
 nltk.download('punkt', force=True)
